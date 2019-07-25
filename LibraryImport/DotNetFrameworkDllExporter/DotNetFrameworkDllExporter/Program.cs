ee3ea66e90d71f5d63f5254f825ae7e15da0ed0b
Dalibor Zeman <Zeman-Dalibor@users.noreply.github.com>
2019-07-26 00:05:53 +0200

    Added .NET Framework dll example and exporter skeleton

---
+++ LibraryImport/DotNetFrameworkDllExporter/DotNetFrameworkDllExporter/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetFrameworkDllExporter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Start(args, Console.In, Console.Out);
+        }
+
+        private static void Start(string[] args, TextReader @in, TextWriter @out)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LibraryImport/DotNetFrameworkDllExporter/DotNetFrameworkExampleDll/ExampleClass1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetFrameworkExampleDll
+{
+    public class ExampleClass1
+    {
+        public static char ExampleStaticVariable;
+
+        public static void ExampleStaticVoidMethod()
+        {
+            throw new NotImplementedException();
+        }
+
+        public long ExampleInstanceVariable;
+
+        public void ExampleVoidMethod()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ExampleParametrizedMethod()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ExampleAutoProperty { get; set; }
+
+        public byte ExampleFullProperty { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
