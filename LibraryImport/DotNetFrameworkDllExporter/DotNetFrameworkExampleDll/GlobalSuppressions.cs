f237e0ddf9bb4a017bebc940806349703233fca8
Dalibor Zeman <Zeman-Dalibor@users.noreply.github.com>
2019-07-27 22:26:40 +0200

    Exporter code style fix

--- LibraryImport/DotNetFrameworkDllExporter/DotNetFrameworkExampleDll/GlobalSuppressions.cs
+++ LibraryImport/DotNetFrameworkDllExporter/DotNetFrameworkExampleDll/GlobalSuppressions.cs
@@ -1,7 +1,7 @@
 ﻿
-// This file is used by Code Analysis to maintain SuppressMessage 
+// This file is used by Code Analysis to maintain SuppressMessage
 // attributes that are applied to this project.
-// Project-level suppressions either have no target or are given 
+// Project-level suppressions either have no target or are given
 // a specific target and scoped to a namespace, type, member, etc.
 
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1401:Fields should be private", Justification = "<Pending>", Scope = "member", Target = "~F:DotNetFrameworkExampleDll.ExampleClass1.ExampleStaticVariable")]
